94c3d0879941ac927ad4b599c0a5e5fc88014b34
Kate Cevora <katecevora@Kates-MacBook-Pro.local>
2023-05-15 12:15:13 +0100

    First commit

---
+++ convertTrainNames.py
@@ -0,0 +1,22 @@
+# Convert the names of the training and test images to have the nnUNet format
+import os
+
+ROOT_DIR = '/Users/katecevora/Documents/PhD/data/AMOS_3D/'
+TRAIN_DIR = os.path.join(ROOT_DIR, "nnUNet_raw/Dataset200_AMOS/imagesTr")
+
+
+def main():
+    # list files in training directory
+    names = os.listdir(TRAIN_DIR)
+
+    for n in names:
+        if n.endswith(".nii.gz"):
+            if not (n.split(".")[0][-1] == '0'):
+                root = n.split(".")[0]
+                n_new = root + "_0000.nii.gz"
+
+                os.rename(os.path.join(TRAIN_DIR, n), os.path.join(TRAIN_DIR, n_new))
+
+
+if __name__ == "__main__":
+    main()
---
+++ run_nnUNet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Launch virtual environment
+source venv/bin/activate
+
+# Set environment variables
+ROOT_DIR='/Users/katecevora/Documents/PhD/data/AMOS_3D/'
+
+export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
+export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
+export nnUNet_results=$ROOT_DIR"nnUNet_results"
+
+echo $nnUNet_raw
+echo $nnUNet_preprocessed
+echo $nnUNet_results
+
+# Run script to generate dataset json
+#python3 generateDatasetJson.py -r $ROOT_DIR -n $DS -t $TASK
+
+# Plan and preprocess data
+nnUNetv2_plan_and_preprocess -d 200 --verify_dataset_integrity
+
+# Train
+nnUNetv2_train 200 3d_fullres 0
