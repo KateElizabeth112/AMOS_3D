5edf64bfd7a52ed9a1434f2963ead7fbb47091c2
Kate Cevora <katecevora@kates-mbp.home>
2023-07-31 09:38:39 +0100

    CHanged slurm setup

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Example of running python script in a batch mode
 #SBATCH -c 4 # Number of CPU Cores
-#SBATCH -p gpushigh # Partition (queue)
+#SBATCH -p gpus # Partition (queue)
 #SBATCH --gres gpu:1 # gpu:n, where n = number of GPUs
-#SBATCH --mem 20G # memory pool for all cores
-#SBATCH --nodelist monal04 # SLURM node
+#SBATCH --mem 32G # memory pool for all cores
+#SBATCH --nodelist lory # SLURM node
 #SBATCH --output=slurm.%N.%j.log # Standard output and error log
 # Launch virtual environment
 source venv/bin/activate
