5ca62a592a657739074f58c085c303968320d552
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-16 10:46:59 +0100

    Added separate predict script

---
+++ predict_nnUNet.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -l walltime=5:00:00
+#PBS -l select=1:ncpus=8:mem=32gb:ngpus=1:gpu_type=RTX6000
+#PBS -N nnUNet_AMOS
+
+cd ${PBS_O_WORKDIR}
+
+# Launch virtual environment
+module load anaconda3/personal
+source activate nnUNetv2
+
+## Verify install:
+python -c "import torch;print(torch.cuda.is_available())"
+
+# Set environment variables
+ROOT_DIR='/rds/general/user/kc2322/home/data/AMOS_3D/'
+DATASET="Dataset701_Set1"
+
+export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
+export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
+export nnUNet_results=$ROOT_DIR"nnUNet_results"
+
+# Inference
+INPUT_FOLDER=$ROOT_DIR"nnUNet_raw/Dataset701_Set1/imagesTs"
+OUTPUT_FOLDER=$ROOT_DIR"inference/Dataset701_Set1/all"
+
+nnUNetv2_predict -i $INPUT_FOLDER -o $OUTPUT_FOLDER -d 701 -c 3d_fullres -f all
