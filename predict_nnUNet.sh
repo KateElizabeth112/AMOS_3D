1a83da9791525b0b05fa0bab25eb17515670fecf
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-16 15:47:24 +0100

    UPdated memory requiremenbts

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=5:00:00
-#PBS -l select=1:ncpus=8:mem=32gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_AMOS
+#PBS -l select=1:ncpus=8:mem=64gb:ngpus=1:gpu_type=RTX6000
+#PBS -N nnUNet_AMOS_predict
 
 cd ${PBS_O_WORKDIR}
 
