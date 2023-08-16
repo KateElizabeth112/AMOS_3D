abc5de0f01db93cd32289149196a446f8f71f25a
Kate Cevora <kate.wilkinson@illumr.com>
2023-08-16 10:35:23 +0100

    Updated run script

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -25,10 +25,10 @@ echo $nnUNet_preprocessed
 echo $nnUNet_results
 
 # Plan and preprocess data
-#nnUNetv2_plan_and_preprocess -d 701 -c 3d_fullres -np 3 --verify_dataset_integrity
+nnUNetv2_plan_and_preprocess -d 703 -c 3d_fullres -np 3 --verify_dataset_integrity
 
 # Train
-nnUNetv2_train 701 3d_fullres all
+nnUNetv2_train 703 3d_fullres all
 
 # Inference
 #INPUT_FOLDER=$ROOT_DIR"nnUNet_raw/Dataset702_Set2/imagesTs"
