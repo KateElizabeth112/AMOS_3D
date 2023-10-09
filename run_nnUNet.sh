bff4fc10077360224e9bbcb33977adb1d49236fc
Kate Cevora <kate.wilkinson@illumr.com>
2023-10-09 15:39:13 +0100

    Updated to 601

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=48:00:00
 #PBS -l select=1:ncpus=12:mem=64gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_AMOS_601
+#PBS -N nnUNet_AMOS_602
 
 cd ${PBS_O_WORKDIR}
 
@@ -14,8 +14,8 @@ python -c "import torch;print(torch.cuda.is_available())"
 
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/home/data/AMOS_3D/'
-DATASET='Dataset601_Fold1'
-TASK=601
+DATASET='Dataset602_Fold1'
+TASK=602
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
