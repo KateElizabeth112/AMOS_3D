10cee7f65fde1a038c640f720398010e54ab9922
Kate Cevora <kate.wilkinson@illumr.com>
2023-10-09 13:41:07 +0100

    501

--- run_nnUNet.sh
+++ run_nnUNet.sh
@@ -14,7 +14,9 @@ python -c "import torch;print(torch.cuda.is_available())"
 
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/home/data/AMOS_3D/'
-DATASET="Dataset701_Set1"
+DATASET='Dataset501_Fold0'
+TASK=601
+
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
@@ -24,14 +26,11 @@ echo $nnUNet_raw
 echo $nnUNet_preprocessed
 echo $nnUNet_results
 
+# Create dataset.json
+python3 generateDatasetJson.py -r $ROOT_DIR -n $DATASET -tc 304
+
 # Plan and preprocess data
-nnUNetv2_plan_and_preprocess -d 703 -c 3d_fullres -np 3 --verify_dataset_integrity
+nnUNetv2_plan_and_preprocess -d $TASK -c 3d_fullres -np 3 --verify_dataset_integrity
 
 # Train
-nnUNetv2_train 703 3d_fullres all
-
-# Inference
-#INPUT_FOLDER=$ROOT_DIR"nnUNet_raw/Dataset702_Set2/imagesTs"
-#OUTPUT_FOLDER=$ROOT_DIR"inference/Dataset702_Set2/all"
-
-#nnUNetv2_predict -i $INPUT_FOLDER -o $OUTPUT_FOLDER -d 702 -c 3d_fullres -f all -device cpu
+nnUNetv2_train $TASK 3d_fullres all
