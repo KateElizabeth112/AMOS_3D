bb14876d9213b6893a88507acb110a31bbcc9246
Kate Cevora <kate.wilkinson@illumr.com>
2023-10-13 13:03:49 +0100

    UPdated to 900

--- predict_nnUNet.sh
+++ predict_nnUNet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l walltime=15:00:00
 #PBS -l select=1:ncpus=15:mem=120gb:ngpus=1:gpu_type=RTX6000
-#PBS -N nnUNet_AMOS_predict_500
+#PBS -N nnUNet_AMOS_predict_900
 
 cd ${PBS_O_WORKDIR}
 
@@ -15,8 +15,8 @@ python -c "import torch;print(torch.cuda.is_available())"
 # Set environment variables
 ROOT_DIR='/rds/general/user/kc2322/home/data/AMOS_3D/'
 
-datasets=("Dataset501_Fold0" "Dataset502_Fold0")
-tasks=(501 502)
+datasets=("Dataset900_Fold4" "Dataset901_Fold4" "Dataset902_Fold4")
+tasks=(900 901 902)
 
 export nnUNet_raw=$ROOT_DIR"nnUNet_raw"
 export nnUNet_preprocessed=$ROOT_DIR"nnUNet_preprocessed"
